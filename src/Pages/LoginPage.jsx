331318c0ece9fe1573398aef1a49ba5cc6a9b510
mehra91 <pm7300779625@gmail.com>
2025-10-29 12:37:35 +0530

    add responsive login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 const LoginPage = () => {
   return (
     <>
-        <div className="  h-full w-auto flex  justify-center">
+        <div className=" h-full w-sm md:w-auto  md:flex  justify-center md:overfloe-x-visible overflow-x-hidden">
             <MostHeader className="-top-10   " />
 
-            <div className="flex h-full mt-0 pt-40 w-3xl items-center justify-center   ">
+            <div className="md:flex h-full mt-0 pt-40 w-3xl items-center justify-center   ">
                     {/* Left Section */}
-                <div className="bg-sky-950  tracking-widest  flex  justify-start pl-5  h-96 w-xs ">
+                <div className="bg-sky-950  tracking-widest hidden  md:flex  justify-start pl-5  h-96 w-xs ">
                         <h1 className="text-white text-5xl font-semibold  leading-snug h-48">
                             Deals on  
                             overstock  
@@ -21,51 +21,52 @@ const LoginPage = () => {
                 </div>
 
                     {/* Right Section */}
-                <div className="flex-1 flex flex-col justify-center px-20">
-                    <div className="max-w-md w-full">
-                        <h2 className="text-2xl font-semibold text-[#162B4E] mb-8 border-b pb-2">
+                <div className=" flex flex-col justify-center h-auto w-sm md:px-20 m-5 ">
+
+                    <div className=" w-sm md:max-w-md h-auto">
+                        <h2 className=" text-xl md:text-2xl font-semibold text-sky-950 mb-8   w-85 md:w-95  border-b pb-2">
                             Sign In and Get Started
                         </h2>
 
                         {/* Email Input */}
-                        <div className="mb-6">
-                            <label className="block text-[#162B4E] text-sm mb-2">Email</label>
+                        <div className="md:mb-6 mb-2">
+                            <label className="block text-[#162B4E] text-sm mb-1">Email</label>
                             <input
                             type="email"
                             placeholder="Enter email"required
-                            className="w-full p-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-400"
+                            className=" p-3 rounded-full   w-85 md:w-95 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-400"
                             />
                         </div>
 
                         {/* Password Input */}
-                        <div className="mb-3 relative">
-                            <label className="block text-[#162B4E] text-sm mb-2">
+                        <div className="md:mb-3 mb-4 relative">
+                            <label className="block text-[#162B4E]  text-sm mb-1">
                             Password
                             </label>
                             <input
                             type="password"
                             placeholder="Enter password"
-                            className="w-full p-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-400"
+                            className=" p-3 rounded-full border border-gray-300  w-85 md:w-95 focus:outline-none focus:ring-2 focus:ring-orange-400"
                             />
-                                <IoIosEye className="absolute right-4 top-10 text-gray-400" />
+                                <IoIosEye className="absolute right-15 md:right-5 top-11 md:top-11 text-gray-400" />
                         </div>
 
                         {/* Stay Logged In & Forgot Password */}
-                        <div className="flex items-center justify-between mb-6">
-                            <label className="flex items-center text-gray-500 text-sm">
+                        <div className="md:flex  items-center justify-between mb-6">
+                            <label className="flex items-center text-gray-500 text-sm ">
                             <input type="checkbox" className="mr-2" />
                             Stay Logged In
                             </label>
                             <a
                             href="#"
-                            className="text-orange-500 font-semibold text-sm hover:underline"
+                            className="text-orange-500 font-semibold text-sm hover:underline "
                             >
                             Forgot Password?
                             </a>
                         </div>
 
                         {/* Login Button */}
-                        <button className="w-full bg-orange-500 text-white py-3 rounded-full font-semibold flex items-center justify-center gap-2 hover:bg-orange-600 transition">
+                        <button className="w-85 md:w-95 bg-orange-500 text-white py-3 rounded-full font-semibold flex items-center justify-center gap-2 hover:bg-orange-600 transition">
                             Log In
                             <span className="text-lg">↩</span>
                         </button>
